b926ddf46a41cce17e4e9c7e0431580d11228a2f
bul.gleb <bul.gleb@gmail.com>
2023-03-02 16:34:38 +0300

    Исправления

--- src/main/java/me/bulkanov/User.java
+++ src/main/java/me/bulkanov/User.java
@@ -4,6 +4,9 @@ public class User {
     public String login;
     public String email;
 
+    public User() {
+    }
+
     public User(String login, String email) {
         this.login = login;
         this.email = email;
--- src/test/java/me/bulkanov/UserTest.java
+++ src/test/java/me/bulkanov/UserTest.java
@@ -7,15 +7,30 @@ import org.junit.jupiter.api.Test;
 public class UserTest {
 
     @Test
-    @DisplayName("When the login not null, we create a user")
-    void getLogin() {
+    @DisplayName("When the login and email not null, we create a user")
+    void getNull() {
         User Alex = new User("Alex","alex@yandex.ru");
         Assertions.assertNotNull(Alex.login);
+        Assertions.assertNotNull(Alex.email);
+        Assertions.assertNotEquals(Alex.login, Alex.email);
     }
     @Test
-    @DisplayName("When the email noy null, we create a user")
+    @DisplayName("When the login and email not equal, we create a user")
+    void getNotEquals() {
+        User Alex = new User("Alex","alex@yandex.ru");
+        Assertions.assertNotEquals(Alex.login, Alex.email);
+    }
+    @Test
+    @DisplayName("When an email address has an @ sign, we create a user")
     void getEmail() {
         User Alex = new User("Alex","alex@yandex.ru");
-        Assertions.assertNotNull(Alex.email);
+        Assertions.assertTrue(Alex.getEmail().contains("@"));
+    }
+    @Test
+    @DisplayName("When the parameters are null, we create an object")
+    void getUser() {
+        User Test = new User();
+        Assertions.assertNull(Test.login);
+        Assertions.assertNull(Test.email);
     }
 }
